f432a9bef685caf492a65de42375ea0ee4c48937
AdrianMP16 <adriananaluca2004@gmail.com>
2026-06-15 00:19:07 -0500

    Modificaiciones vercel

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,6 +1,6 @@
 // backend/config/firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("../firebase-credentials.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
